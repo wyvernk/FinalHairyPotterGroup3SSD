b1cade8844e490e48deafcb516faf6c846e24fb9
motorfireman <motorfireman@gmail.com>
2024-07-05 02:20:46 +0800

    disabling security configuration
    disabling security configuration

--- HairyPotterEcommerce/source-code/Ecommerce/Ecommerce.Domain/Models/Settings/StockConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ecommerce.Domain.Models;
-public class StockConfiguration
-{
-    public bool IsLowStockNotificationEnabled { get; set; } = false;
-    public bool IsOutOfStockNotificationEnabled { get; set; } = false;
-    public int? LowStockThreshold { get; set; } = 0;
-    public int? OutOfStockThreshold { get; set; } = 0;
-    public bool IsOutOfStockItemHidden { get; set; } = false;
-}
